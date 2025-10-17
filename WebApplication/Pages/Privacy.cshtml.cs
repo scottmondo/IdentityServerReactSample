d5b68c5409f32ab9ce6b11ab030fee7f94ae66b0
Scott McFarlane <scott.mcfarlane@mondo.com.au>
2025-10-18 09:19:03 +1100

    Add razor web app

---
+++ WebApplication/Pages/Privacy.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication.Pages;
+
+public class PrivacyModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult OnGet()
+    {
+        Console.WriteLine("Bunny: " + User.Claims.Select(x => $"{x.Type}: {x.Value}").Aggregate((a, b) => a + ", " + b));
+        var claimTwoFactorEnabled = User.Claims.FirstOrDefault(t => t.Type == "amr");
+
+        if (claimTwoFactorEnabled != null && "mfa".Equals(claimTwoFactorEnabled.Value))
+        {
+            // You logged in with MFA, do the admin stuff
+        }
+        else
+        {
+            return Redirect("/Error");
+        }
+
+        return Page();
+  }
+}
---
+++ WebApplication/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Logging;
+
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(authenticationOptions =>
+    {
+        authenticationOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        authenticationOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddOpenIdConnect(openIdConnectOptions =>
+    {
+        openIdConnectOptions.Authority = builder.Configuration["Authentication:Authority"];
+        openIdConnectOptions.ClientId = builder.Configuration["Authentication:ClientId"];
+        openIdConnectOptions.ClientSecret = builder.Configuration["Authentication:ClientSecret"];
+        openIdConnectOptions.GetClaimsFromUserInfoEndpoint = true;
+        openIdConnectOptions.RequireHttpsMetadata = false;
+        openIdConnectOptions.ResponseType = "code";
+        openIdConnectOptions.SaveTokens = true;
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+
+    IdentityModelEventSource.ShowPII = true;
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapRazorPages();
+
+app.Run();
